e8a8a69bfb760d6ef353078d7d87d943bce70717
Mark Daniels <mark_daniels1982@hotmail.com>
2015-01-13 22:34:46 -0800

    Complete Part 2

--- assoc_lab/db/schema.rb
+++ assoc_lab/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114043657) do
+ActiveRecord::Schema.define(version: 20150114062334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20150114043657) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "book_libraries", force: :cascade do |t|
+    t.integer  "library_id"
+    t.integer  "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
@@ -33,6 +40,14 @@ ActiveRecord::Schema.define(version: 20150114043657) do
     t.integer  "author_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "libraries", force: :cascade do |t|
+    t.string   "name"
+    t.string   "city"
+    t.string   "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
